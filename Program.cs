1ecbe3ca034e327631ffbd9f955a98fff94709b8
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-10-05 02:18:24 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\tPrograma para mostrar todos los años bisiestos desde 1900");
+Console.WriteLine("\t---------------------------------------------------------\n");
+Console.WriteLine("Años bisiestos: \n");
+int contador = 1;
+
+for (float i = 1900; i <= 2023; i++)
+{
+    if (((i % 4 == 0) & (i % 100 == 0) & (i % 400 == 0)) || ((i % 4 == 0) & (i % 100 != 0)))
+    {
+        Console.WriteLine($"Año {contador}: {i}");
+        contador++;
+    }
+}
+
+    
